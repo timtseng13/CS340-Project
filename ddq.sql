4642dc4761a1770353a3f2f812f340268145475b
camman <c.grein123@gmail.com>
2019-08-13 12:35:02 -0700

    Update ddq.sql

--- ddq.sql
+++ ddq.sql
@@ -18,3 +18,16 @@ LOCK TABLES `TV_show` WRITE;
 INSERT INTO `TV_show`(`showTitle`, `Genre`, `producer`,`episode_Count`, `musicID`, `movieID`) VALUES 
 (:showtitle, :genre, :producer, :episode_count, :musicID, movieID),
 UNLOCK TABLES; 
+
+--using the select search on movie, will be able to used on any of the catagories 
+SELECT * FROM Movie 
+WHERE Movie.movieTitle = :userinput; 
+
+--delete a music 
+LOCK TABLES `Music` WRITE; 
+DELETE FROM `Music` WHERE Music.musicID = :buttnouserid
+
+--update an entry, there will be an update button next to each enteity on the website when pressed, will allow you to change them. 
+UPDATE movie 
+SET movieTitle = :user_input, Genre = :user_input 
+WHERE Movie.movieID = :movieID
